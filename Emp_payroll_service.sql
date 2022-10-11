9b4eeac7faf1c37cd7698656581eb898976dd275
AnjaliSahu <anjalisahu1996@gmail.com>
2022-10-11 14:04:33 +0530

    Between Clause

--- Emp_payroll_service.sql
+++ Emp_payroll_service.sql
@@ -17,3 +17,18 @@ insert into Payroll_Service (Name,Salary,Startdate)values('Sam',10000,GETDATE())
 
 select * from Payroll_Service;
 
+insert into Payroll_Service (Name,Salary,StartDate)values('Ash',40000,'2021-09-08'),
+														 ('Anji',38000,'2019-12-05');
+
+
+update Payroll_Service 
+set StartDate='2020-01-01' where ID=1;
+
+update Payroll_Service 
+set StartDate='2018-01-01' where ID=2;
+
+
+update Payroll_Service 
+set StartDate='2017-01-01' where ID=3;
+
+select salary,Name,ID,StartDate from Payroll_Service where StartDate between CAST('2020-01-01'AS date)And GETDate();
